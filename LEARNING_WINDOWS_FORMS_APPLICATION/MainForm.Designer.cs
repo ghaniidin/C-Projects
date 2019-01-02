312fd3c64909bd2165c142789698fc4a40812491
ghaniidin <ghani.idin@gmail.com>
2019-01-02 11:26:07 +0330

    Add project files.

---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(12, 41);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 1;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.SteelBlue;
+			this.ClientSize = new System.Drawing.Size(379, 321);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.Name = "MainForm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Main";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+	}
+}
---
+++ LEARNING_WINDOWS_FORMS_APPLICATION/MainForm.cs
@@ -0,0 +1,35 @@
+﻿namespace LEARNING_WINDOWS_FORMS_APPLICATION
+{
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		private void f1()
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void f2(object sender)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void f3(System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void f4(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+
+		private void button1_Click(object sender, System.EventArgs e)
+		{
+			System.Windows.Forms.MessageBox.Show("Hello, World!");
+		}
+	}
+}
